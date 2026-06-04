d82b8e0e6a0eb4e364df1698e81d5399f6f8ef38
krrsalmane <karroumsalmane@gmail.com>
2026-06-04 09:54:04 +0100

    first commit

---
+++ src/main/java/command/CaiseMayaGroup/CaiseMayaGroupApplication.java
@@ -0,0 +1,12 @@
+package command.CaiseMayaGroup;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaiseMayaGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaiseMayaGroupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/command/CaiseMayaGroup/CaiseMayaGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package command.CaiseMayaGroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaiseMayaGroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
